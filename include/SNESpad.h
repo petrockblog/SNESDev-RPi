5eab8b47d96615d4aed62bf0f6835adc884bd9c8
5StringsOfDoom <thestompbox1983@gmail.com>
2012-08-27 23:08:24 +0200

    Removed SNES buttons and left NES variants

--- include/SNESpad.h
+++ include/SNESpad.h
@@ -49,18 +49,20 @@ typedef struct {
 } buttonstates;
 
 /* bit masks for checking the button states */
-#define SNES_B       0x01
-#define SNES_Y       0x02
+//#define SNES_B       0x01
+//#define SNES_Y       0x02
+#define SNES_A       0x01
+#define SNES_B       0x02
 #define SNES_SELECT  0x04
 #define SNES_START   0x08
 #define SNES_UP      0x10
 #define SNES_DOWN    0x20
 #define SNES_LEFT    0x40
 #define SNES_RIGHT   0x80
-#define SNES_A       0x100
-#define SNES_X       0x200
-#define SNES_L       0x400
-#define SNES_R       0x800
+//#define SNES_A       0x100
+//#define SNES_X       0x200
+//#define SNES_L       0x400
+//#define SNES_R       0x800
 
 /* check the state of each button of each controller */
 void updateButtons( snespad *pad, buttonstates* buttons );
